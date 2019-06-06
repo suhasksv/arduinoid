075556b502c1c2a6f3165b00ebfd0b6f5d732327
Suhas <suhas.ksv@gmail.com>
2019-06-06 21:45:41 +0530

    seven segment display
    seven segment display program

---
+++ ex16/ex16.ino
@@ -0,0 +1,40 @@
+const int a = 2;
+const int b = 3;
+const int c = 4;
+const int d = 5;
+const int e = 6;
+const int f = 7;
+const int g = 8;
+const int dp = 9;
+
+void setup() {
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(dp, OUTPUT);
+}
+
+//void loop() {
+ // two();
+  //delay(1000);
+  //five();
+  //delay(1000);
+//}
+
+void loop() {
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH); 
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(dp, HIGH);
+  delay(500000);
+}
+
